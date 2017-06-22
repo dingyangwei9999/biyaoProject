0244ab862cd5b4b1657354967a1730f0086d9853
Issac <326748283@qq.com>
2017-06-22 10:26:19 +0800

    添加global.js文件，修改主色调

---
+++ web/src/global.js
@@ -0,0 +1,9 @@
+const erp = erp || {};
+
+var base = 'http://10.3.133.50:8888/';
+erp.baseUrl = base;
+erp.webappUrl = base+'webapp/';
+erp.htmlUrl = base+'webapp/html/';
+erp.uploadUrl = base+'upload/';
+
+export default erp
